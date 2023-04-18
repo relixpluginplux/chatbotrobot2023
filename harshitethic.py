9a9ed95984371a00e7cd1bfadae031aa8f3dd0e5
relixpluginplux <106030930+relixpluginplux@users.noreply.github.com>
2023-04-18 09:11:14 -0400

    Update harshitethic.py

--- harshitethic.py
+++ harshitethic.py
@@ -7,7 +7,7 @@ import openai
 #OpenAI API key
 aienv = os.getenv('OPENAI_KEY')
 if aienv == None:
-    openai.api_key = "ENTER YOUR API KEY HERE"
+    openai.api_key = "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"
 else:
     openai.api_key = aienv
 print(aienv)
@@ -15,7 +15,7 @@ print(aienv)
 #Telegram bot key
 tgenv = os.getenv('TELEGRAM_KEY')
 if tgenv == None:
-    tgkey = "ENTER YOUR TELEGRAM TOKEN HERE"
+    tgkey = "5819525125:AAHYssfMvoebrHJLKUWJRAEXxSyYnwvrLI8"
 else:
     tgkey = tgenv
 print(tgenv)
